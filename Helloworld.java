b95503fbf4a68ed20f106ef3cb55b75a1156d4f2
YASHzip <singhyashvardhn402@gmail.com>
2026-01-31 21:47:11 +0530

    Updated file

--- Helloworld.java
+++ Helloworld.java
@@ -14,9 +14,6 @@
 
 class Helloworld{
     public static void main(String[] args){
-        int arr[] = {1, 2, 3, 4 , 5};
-        for(int i = 0; i < 5; i++){
-            System.out.println(arr[i]);
-        }
+        System.out.println("Hello World");
     }
 }
